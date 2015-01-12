12c4e62c978bb3107254a7a6b882e8e296f652a6
KehaoWu <kehao.wu@gmail.com>
2015-01-12 13:57:48 -0500

    First Commit

---
+++ GenerateSeq.R
@@ -0,0 +1,36 @@
+randomOne = function()
+{
+  repo = c("A","B","C","D","E","F","G","H",
+           "I","J","K","L","M","N","O","P",
+           "Q","R","S","T","U","V","W","X",
+           "Y","Z")
+  return(repo[sample(1:26,size = 1)])
+}
+
+generateSeq = function(destSeq="TOBEORNOTTOBE",Niter=5000)
+{
+  timesSet = vector(mode = "numeric",length = Niter)
+  pb = txtProgressBar(min = 1,max = Niter,style = 3)
+  destSeq = strsplit(destSeq,split = "")[[1]]
+  for(i in 1:Niter)
+  {
+    times = 0  ## Times of random
+    seq = vector(mode = "character",length = length(destSeq))
+    while(!all(destSeq==seq))
+    {
+      times = times + 1
+      unequalIndex = (1:length(destSeq))[destSeq!=seq]
+      lapply(unequalIndex,FUN = function(x)
+      {
+        seq[x] <<- randomOne()
+      })
+    }
+    timesSet[Niter] = times
+    setTxtProgressBar(pb = pb,value = i)
+  }
+  cat("\n")
+  averageTimes = mean(timesSet)
+  cat("Average times:",averageTimes,"\n")
+  return(averageTimes)
+}
+generateSeq()
---
+++ README.md
@@ -0,0 +1,2 @@
+# 模拟进化
+根据http://mindhacks.cn/2007/12/02/probability-theory-in-evolution/ 提出的进化理论模拟"TOBEORNOTTOBE"的生成的次数
